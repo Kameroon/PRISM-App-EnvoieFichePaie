5701dc01547f0139388740ae658f03d2f3831cb8
DAVID MIKE <aidipifr@hotmail.com>
2019-08-30 22:48:15 +0200

    Add Delegate commnd project

---
+++ WpfAppSimpleDelegateCommand/ViewModels/MainWindowModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAppSimpleDelegateCommand.Commands;
+
+namespace WpfAppSimpleDelegateCommand.ViewModels
+{
+    public class MainWindowModel
+    {
+        public DelegateCommand TestCmd { get; set; }
+        public DelegateCommand TestCmd2 { get; set; }
+
+
+        public MainWindowModel()
+        {
+            // --  Show send mail form command  --            
+            TestCmd = new DelegateCommand(x => TestMethod());
+
+            TestCmd2 = new DelegateCommand(x => TestMethod2());
+        }
+
+        private void TestMethod()
+        {
+            
+        }
+
+        private void TestMethod2()
+        {
+
+        }
+    }
+}
